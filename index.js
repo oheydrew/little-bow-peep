cc1ab6663058fa2c10b1f25502199daad68d8839
Michael Winter <mcwinter07@gmail.com>
2018-06-27 14:49:21 +1000

    First commit

---
+++ index.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+const Express = require('express')
+const bodyParser = require('body-parser')
+const cors = require('cors')
+
+
+const app = new Express()
+
+// TODO: import routers
+
+// TODO: app.use Middleware(bodyparser, cors)
+
+// TODO: Create routes
+
+// TODO: Run server
+
