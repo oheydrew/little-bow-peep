90795997679e4420a176f84dd1a23da20b267610
Michael Winter <mcwinter07@gmail.com>
2018-06-27 15:04:24 +1000

    Add express server listen

--- index.js
+++ index.js
@@ -14,3 +14,12 @@ const app = new Express()
 
 // TODO: Run server
 
+app.listen(process.env.SERVER_PORT, () => {
+    let currentTime = new Date(Date.now()).toLocaleTimeString()
+    console.log(`${currentTime}: express server listening on port ${process.env.SERVER_PORT}🎀`)
+})
+    .on('error', (error) => {
+        console.log('Your ties undone!')
+        console.log(error)
+    })
+
