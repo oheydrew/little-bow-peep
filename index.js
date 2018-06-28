d5c2688649ba0882d2253d2384d82bcf8329f8f8
Michael Winter <mcwinter07@gmail.com>
2018-06-28 16:31:25 +1000

    Cleanup: Remove whitespace

--- index.js
+++ index.js
@@ -2,33 +2,26 @@ require('dotenv').config()
 const Express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const auth = require('./auth/authMiddleware')
+const logger = require('./middleware/logger')
 
 // DONE: import routers 
-
-const logger = require('./middleware/logger')
 const authRouter = require('./auth/authRouter')
 const userRouter = require('./routes/userRouter')
 const bowtieRouter = require('./routes/bowtieRouter')
-const auth = require('./auth/authMiddleware')
 
 const app = new Express()
 
 // DONE: app.use Middleware(bodyparser, cors)
-
-
-
 app.use(bodyParser.json())
 app.use(cors())
 app.use(logger)
 
 // TODO: Create routes
-
 app.use('/api/auth', authRouter)
 app.use('/api/bowtie', auth.tokenHandler, bowtieRouter)
 
-
 // TODO: Run server
-
 app.listen(process.env.SERVER_PORT, () => {
     let currentTime = new Date(Date.now()).toLocaleTimeString()
     console.log(`🎀 ${currentTime}: express server listening on port ${process.env.SERVER_PORT} 🎀`)
